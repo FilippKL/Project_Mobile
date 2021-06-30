f98d56c3aca4301817a91e352e1e94567e859c41
Filipp Krupnov <philkrupnov@gmail.com>
2021-06-30 08:53:39 +0600

    Initial Commit

---
+++ Project_Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_Mobile
+//
+//  Created by Filipp Krupnov on 30/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
